53b3386bf45d41c3b366de397292bfc2164138a4
Róger José Ulate Rivera <roger.p.designe357@gmail.com>
2020-08-23 23:41:13 -0600

    Prueba

---
+++ first-node-experience/dist/test.dev.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var strawberryCount = 20;
+
+if (strawberryCount > 10) {
+  console.log("You have enough strawberries");
+} else {
+  console.log("You do not have enough berries.");
+}
