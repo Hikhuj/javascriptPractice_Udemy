7e7b7e966ad17d1b0043f37c97b22ceb25d7fcf8
Róger José Ulate Rivera <roger.p.designe357@gmail.com>
2020-08-30 21:27:19 -0600

    Creando servidor de JS

--- first-node-experience/dist/test.dev.js
+++ first-node-experience/dist/test.dev.js
@@ -1,9 +1,16 @@
 "use strict";
 
-var strawberryCount = 20;
+var http = require("http"); // Crear un servidor que escuche lo que vamos a escribir
+// Objeto
+// Es bueno almacenarlo en una variable para que sea mas facil, obviamente, manejarlo
+// creando una funcion anonima con dos parametros que dan mejor sentido como request = req
+// y response = res
 
-if (strawberryCount > 10) {
-  console.log("You have enough strawberries");
-} else {
-  console.log("You do not have enough berries.");
-}
+
+var rogerApp = http.createServer(function (req, res) {
+  console.log(req.url);
+  res.end("Hola, bienvenidos a mi servidor webservice.");
+}); // Metodo que le permite al servidor escuchar lo que reciba de un request hecho a este
+// Se le debe pasar un argumento que le permita a uno donde debe escuchar, en que puerto
+
+rogerApp.listen(3000);
--- first-node-experience/test.js
+++ first-node-experience/test.js
@@ -1,7 +1,15 @@
-let strawberryCount = 20
+let http = require("http")
 
-if (strawberryCount > 10) {
-    console.log("You have enough strawberries")
-} else {
-    console.log("You do not have enough berries.")
-}
+// Crear un servidor que escuche lo que vamos a escribir
+// Objeto
+// Es bueno almacenarlo en una variable para que sea mas facil, obviamente, manejarlo
+// creando una funcion anonima con dos parametros que dan mejor sentido como request = req
+// y response = res
+let rogerApp = http.createServer(function(req, res) {
+    console.log(req.url)
+    res.end("Hola, bienvenidos a mi servidor webservice.")
+})
+
+// Metodo que le permite al servidor escuchar lo que reciba de un request hecho a este
+// Se le debe pasar un argumento que le permita a uno donde debe escuchar, en que puerto
+rogerApp.listen(3000)
